316f171c6d75ee66a36cc4d576447035e5aaf0f8
William Davis <liamdavis@tuta.io>
2021-02-20 20:28:19 -0700

    added favicon and plausible analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,34 @@
 import { CSSReset, ChakraProvider } from "@chakra-ui/react";
 import { appTheme } from "src/theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={appTheme}>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>liamdavis.dev</title>
+
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2280%22>🌱</text></svg>"
+        />
+
+        {/* Plausible Analytics script */}
+        {typeof window !== "undefined" &&
+          window?.location?.hostname === "liamdavis.dev" && (
+            <script
+              async
+              defer
+              data-domain="liamdavis.dev"
+              src="https://plausible.io/js/plausible.js"
+            />
+          )}
+      </Head>
+      <ChakraProvider theme={appTheme}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
